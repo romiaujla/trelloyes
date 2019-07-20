9ded8dbaebce6ef5137d7667d36ffb6f4f97254a
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-07-20 11:17:01 -0400

    creating basic html file to convert to react components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './App.css';
 
+
+function Header(props){
+  return (
+    <header class="App-header">
+      <h1>Trelloyes!</h1>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div className="App">
---
+++ src/composition/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Card(props){
+    return (
+        <div className={props.className}>
+
+        </div>
+    );
+}
+
+export default Card;
